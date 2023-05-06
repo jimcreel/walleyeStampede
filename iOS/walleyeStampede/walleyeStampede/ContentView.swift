613d176051778fe23d3d6eb93e9b176e58937c8b
Jim <jim.creel@gmail.com>
2023-05-06 08:33:36 -0700

    initial commit

---
+++ iOS/walleyeStampede/walleyeStampede/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  walleyeStampede
+//
+//  Created by James Creel on 5/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS/walleyeStampede/walleyeStampede/walleyeStampedeApp.swift
@@ -0,0 +1,17 @@
+//
+//  walleyeStampedeApp.swift
+//  walleyeStampede
+//
+//  Created by James Creel on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct walleyeStampedeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
