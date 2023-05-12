f0a52f58b2d28f8e65547454d196cac5df5083f2
jimcreel <jim.creel@gmail.com>
2023-05-12 16:43:37 -0700

    wireframing and styling

--- WalleyeStampede/src/components/App/index.js
+++ WalleyeStampede/src/components/App/index.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Nav from '../Nav';
+import Tournament from '../Tournament';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Nav />
+      <Text>This app is for the Walleye Stampede. Please deposit $10,000</Text>
+      <Tournament></Tournament>
       <StatusBar style="auto" />
     </View>
   );
---
+++ WalleyeStampede/src/components/Tournament/index.jsx
@@ -0,0 +1,31 @@
+import {View, Text, StyleSheet } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown'
+
+const data = [
+        "Walleye Stampede",
+        "Walleye Whirl",
+        "Fish Fling",
+      
+
+export default function Tournament() {
+    return (
+        <View>
+            <Text>Tournament</Text>
+            <SelectDropdown
+                data={data}
+                onSelect={(selectedItem, index) => {
+                    console.log(selectedItem, index)
+                }}
+                defaultButtonText={"Select Tournament"}
+                buttonTextAfterSelection={(selectedItem, index) => {
+                    return selectedItem
+                }}
+                rowTextForSelection={(item, index) => {
+                    return item
+                }}
+            />
+
+        </View>
+        
+    )
+}
